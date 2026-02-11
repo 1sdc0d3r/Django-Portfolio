61765b72a544438e8e9051ae9459d826846eac42
Braden <fish0859@gmail.com>
2026-02-10 21:21:58 -0700

    fixed blog/admin model registrations. ran migrations

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Hobbies, Portfolio
 
 # Register your models here.
-admin.site.register(Hobbies, Portfolio)
+admin.site.register(Hobbies)
+admin.site.register(Portfolio)
