3c9a72252f248f8ec2f19a599c1a1a6cd6c87336
Braden <fish0859@gmail.com>
2026-02-20 18:21:49 -0700

    added templates

--- blog/views.py
+++ blog/views.py
@@ -6,21 +6,7 @@ def greet_user(req):
    return HttpResponse("Hello World")
 
 def home(req):
-    return HttpResponse(f'''<div style='padding:2em;'><p>
-                I am an avid fisherman with a passion to develop software. As
-                technology evolves, we must learn to evolve with it. This is
-                prominent for a successful future.
-              </p>
-              <p>
-                Change is inevitable, there will always be something that is
-                going to try to hold you back. When there is a new problem, I
-                find a new solution. This is the only way to progress.
-              </p>
-            <p>
-              Today is a new day.
-              <br />I love being a source of color in a binary world!
-            </p>
-            </div>''')
+    return render(req, 'blog/home.html')
 
 def hobbies(req):
     hobby_list=Hobbies.objects.all()
@@ -32,4 +18,4 @@ def portfolio(req):
     return HttpResponse(Portfolio.objects.all())
 
 def contact(req):
-    return HttpResponse("bradenbell@mail.weber.edu")
+    return render(req, 'blog/contact.html')
